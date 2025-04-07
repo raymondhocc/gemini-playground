96f2edc52ac7f600d006b0db1c03bca888fa05a1
raymondhocc <raymondhocc@users.noreply.github.com>
2025-04-07 13:37:31 +0800

    Update main.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -284,7 +284,7 @@ async function connectToWebsocket() {
         micButton.disabled = false;
         cameraButton.disabled = false;
         screenButton.disabled = false;
-        logMessage('Connected to Gemini 2.0 Flash Multimodal Live API', 'system');
+        logMessage('Connected to NTTCA LLMs 2.5 Multimodal Live API', 'system');
     } catch (error) {
         const errorMessage = error.message || 'Unknown error';
         Logger.error('Connection error:', error);
